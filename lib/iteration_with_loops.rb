c67d605875a87b74dd4add95e54b3206e7e0715e
menachem m margolis <margolismendel5728@gmail.com>
2020-11-26 19:56:30 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,5 @@
 def find_min_in_nested_arrays(src)
-   
+
    new_array =[]
    row_index = 0
    while row_index <src.count do
@@ -16,5 +16,3 @@ def find_min_in_nested_arrays(src)
    end
    p new_array
 end
-
-
